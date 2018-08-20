e0a7f5f7b7f4e2bb20160a107c67bdbbf040d886
a.andreenkoa <a.andreenko@qpdev.ru>
2018-08-20 01:19:04 -0700

    Добавила проект с автотестами.

---
+++ WBS.Selenium/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace WBS.Selenium
+{
+    [XmlType("user")]
+    public class User
+    {
+        [XmlElement("name")]
+        public string Name { get; set; }
+        [XmlElement("login")]
+        public string Login { get; set; }
+        [XmlElement("password")]
+        public string Password { get; set; }
+    }
+}
