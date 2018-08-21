b8dc7172d051ff3e93b9e1d29fe971a80976d7e1
d.tarasova <d.tarasova@02DEV11>
2018-08-21 15:39:49 +0300

    conj

---
+++ WBS.Selenium/Controllers/UIControllers/UIController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace WBS.Selenium.Controllers.UIControllers
+{
+    public abstract class UIController
+    {
+        protected By locator;
+        protected Context context;
+        protected string id;
+        protected bool waitPostback;
+        protected bool waitPageChanged;
+
+        public virtual void Initialize(Context context, string id, bool waitPostback = false, Dictionary<string, string> parameters = null)
+        {
+            this.context = context;
+            this.id = id;
+            this.waitPostback = waitPostback;
+            if (parameters == null || !parameters.ContainsKey("waitPageChanged") || parameters["waitPageChanged"] == "false")
+                waitPageChanged = false;
+            else
+                waitPageChanged = true;
+        }
+        public bool IsVisible()
+        {
+            try
+            {
+                IWebElement e = context.Wait.Until(ExpectedConditions.ElementIsVisible(locator));
+                return e.Displayed;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
