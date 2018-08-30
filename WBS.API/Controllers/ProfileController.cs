8835aacafe93a7716223b3680f379d7f6845a1ae
Maxim Vlasov <vlasov_ms@list.ru>
2018-08-30 17:04:14 +0300

    WBS-53. Правка пейджинга пользователей

--- WBS.API/Controllers/ProfileController.cs
+++ WBS.API/Controllers/ProfileController.cs
@@ -129,14 +129,14 @@ namespace WBS.API.Controllers
             _logger.LogInformation("Get information about profiles on page");
             var data = _dal.GetUsers()
                         .Select(u => new ProfileViewModel(u))
-                        .ToList()
-                        .OrderBy(f => f.Id)
-                        .Skip((currentPage) * pageSize)
-                        .Take(pageSize);
+                        .ToList();
+            var dataToPage = data.OrderBy(f => f.Id)
+                            .Skip((currentPage) * pageSize)
+                            .Take(pageSize);
             _logger.LogInformation("Getting information is succesfull");
             return Ok(new DataWithPaginationViewModel<ProfileViewModel>
             {
-                Data = data,
+                Data = dataToPage,
                 Pagination = new Pagination { CurrentPage = currentPage, ElementsPerPage = pageSize, ElementsCount = data.Count() }
             });
         }
