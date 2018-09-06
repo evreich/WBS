dbfd5f077e2002193b3da3e6716a63ab6872fd4c
Maxim Vlasov <vlasov_ms@list.ru>
2018-09-06 13:03:41 +0300

    Добавлен костяк по работе над дескрипторам

---
+++ WBS.DAL/Descriptors/CategoryGroupsDescriptor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WBS.DAL.Data.Classes;
+using WBS.DAL.Data.Models;
+
+namespace WBS.DAL.Descriptors
+{
+    public class CategoryGroupsDescriptor : Descriptor
+    {
+        const string titleName = "title";
+        [FieldName(Name = titleName)]
+        private readonly FieldInfo _title;
+
+        const string codeName = "code";
+        [FieldName(Name = codeName)]
+        private readonly FieldInfo _code;
+
+        public CategoryGroupsDescriptor(
+            //BudgetPlanDAL dal, 
+            object typeID,
+            List<string> roles
+            ) : base(
+                //dal, 
+                typeID,
+                roles
+                )
+        {
+            if (!CanAccessToObject())
+                throw new TypeAccessException();
+
+            _title = new FieldInfo("Название", titleName, true, CanEditField(titleName));
+            _code = new FieldInfo("Код", codeName, true, CanEditField(codeName));
+        }
+    }
+}
---
+++ WBS.DAL/Descriptors/FieldInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WBS.DAL.Descriptors
+{
+    internal struct FieldInfo
+    {
+        public string Label { get; set; }
+        public string Name { get; set; }
+        public bool IsVisiable { get; set; }
+        public bool CanEdit { get; set; }
+
+        public FieldInfo(string label, string name, bool isVisiable, bool canEdit)
+        {
+            Label = label;
+            Name = name;
+            IsVisiable = isVisiable;
+            CanEdit = canEdit;
+        }
+    }
+}
