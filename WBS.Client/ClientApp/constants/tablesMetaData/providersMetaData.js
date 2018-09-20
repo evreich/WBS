ce4529fb37e6e5593d65769d773f34fd4ead2f15
Мария Митюрева <m.mityureva@qpdev.ru>
2018-09-20 19:16:55 +0300

    правка providersMetaData

--- WBS.Client/ClientApp/constants/tablesMetaData/providersMetaData.js
+++ WBS.Client/ClientApp/constants/tablesMetaData/providersMetaData.js
@@ -8,8 +8,8 @@ export default {
             sortable: true
         },
         {
-            header: 'Профиль',
-            field: 'profiles',
+            header: 'Тех. службы',
+            field: 'providersTechnicalServices',
             sortable: true
         }
     ]
