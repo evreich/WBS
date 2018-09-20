a6cf862d4f171dd9b89a0a97e9532f647be4c3b6
alex6524i <alex6524i@gmail.com>
2018-09-20 17:55:28 +0300

    WBS-67 дропдаун выбора сита

--- WBS.Client/ClientApp/components/complex/BudgetPlans.jsx
+++ WBS.Client/ClientApp/components/complex/BudgetPlans.jsx
@@ -1,20 +1,29 @@
 ﻿import React from "react";
 
+import SiteSelect from 'containers/textFields/selects/SiteSelect';
+
 class BudgetPlans extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
-            budgetPlanId: null,
-            siteId: null
+            budgetPlanId: 0,
+            siteId: 0
         }
     }
 
-    render(){
+    onSiteChanged = e => {
+        this.setState({
+            siteId : e.target.value
+        });
+    }
+
+    render() {
+        const { siteId, budgetPlanId } = this.state;
         return <>
             <h1>Budget plans table</h1>
-            <h1>Site chooser</h1>
-            <h1>Budget plan detalization</h1>
-            </>;
+            <SiteSelect value={siteId} label="Выберите сит" selectionChanged={this.onSiteChanged} />
+            { !!siteId && !!budgetPlanId && <h1>Budget plan detalization</h1> } 
+        </>;
     }
 }
 
