dccd559b0ac6e7e52106071c35df06640c0c2d82
alex6524i <alex6524i@gmail.com>
2018-09-20 16:35:38 +0300

    WBS-67 контейнеры для БП и заявок с заглушками

---
+++ WBS.Client/ClientApp/components/complex/BudgetPlans.jsx
@@ -0,0 +1,21 @@
+﻿import React from "react";
+
+class BudgetPlans extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            budgetPlanId: null,
+            siteId: null
+        }
+    }
+
+    render(){
+        return <>
+            <h1>Budget plans table</h1>
+            <h1>Site chooser</h1>
+            <h1>Budget plan detalization</h1>
+            </>;
+    }
+}
+
+export default BudgetPlans;
