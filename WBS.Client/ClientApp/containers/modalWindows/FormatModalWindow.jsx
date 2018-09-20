819f2cb00b24809d720872e40cce707155dfc0fa
Maxim Vlasov <vlasov_ms@list.ru>
2018-09-20 14:09:57 +0300

    Исправление по пути в модальных окнах, исправление метода get в общем контроллере, удаление его в provider controller

--- WBS.Client/ClientApp/containers/modalWindows/FormatModalWindow.jsx
+++ WBS.Client/ClientApp/containers/modalWindows/FormatModalWindow.jsx
@@ -4,4 +4,4 @@ import objectTypes from 'constants/objectTypes';
 
 const COMPONENT = 'FormatModalWindow';
 
-export default modalWindowFactory(api.categoryGroups, COMPONENT, objectTypes.format);
+export default modalWindowFactory(api.formats, COMPONENT, objectTypes.format);
